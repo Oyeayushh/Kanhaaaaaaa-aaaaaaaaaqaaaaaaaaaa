612804f332da24afdcc32a050fdeb8fe6224912c
𝐀ʏᴜsʜ 𝐊ᴜᴍᴀʀ 𝐘ᴀᴅᴀᴠ 💞 <pragati.prajapati2009@gmail.com>
2026-07-09 06:21:39 +0530

    feat: complete kanhaCall in KanhaMusic/core/__init__.py

--- KanhaMusic/core/__init__.py
+++ KanhaMusic/core/__init__.py
@@ -1,5 +1,5 @@
 from .bot import app
 from .userbot import assistant
-from .call import call_py
+from .call import call_py, kanhaCall
 
-__all__ = ["app", "assistant", "call_py"]
+__all__ = ["app", "assistant", "call_py", "kanhaCall"]
